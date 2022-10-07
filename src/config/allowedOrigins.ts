c123111cf1acc22ca1c165c3340c297cc0b16b94
th4r1k <th4r1k@gmail.com>
2022-10-07 12:31:40 -0300

    feat: cors webgranjeiros domain

--- src/config/allowedOrigins.ts
+++ src/config/allowedOrigins.ts
@@ -1,5 +1,5 @@
 // export const allowedOrigins = ["http://localhost:3001", "https://webgranjeiros.vercel.app/"];
 export const allowedOrigins = [
-  "http://localhost:3001",
+  //   "http://localhost:3001",
   "https://webgranjeiros.vercel.app/",
 ];
