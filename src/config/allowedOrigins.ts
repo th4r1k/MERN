43090fcc8fca696b5778bb957a57c9a04436dbd4
th4r1k <th4r1k@gmail.com>
2022-10-11 12:41:33 -0300

    test: cors

--- src/config/allowedOrigins.ts
+++ src/config/allowedOrigins.ts
@@ -1,5 +1,5 @@
-// export const allowedOrigins = ["http://localhost:3001", "https://webgranjeiros.vercel.app/"];
-export const allowedOrigins = [
-  "http://localhost:3001",
-  "https://webgranjeiros.vercel.app",
-];
+export const allowedOrigins = ["https://webgranjeiros.vercel.app/"];
+// export const allowedOrigins = [
+//   "http://localhost:3001",
+//   "https://webgranjeiros.vercel.app",
+// ];
